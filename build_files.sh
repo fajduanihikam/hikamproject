6926ab16f7eda97c808b0a1dcdb5452eace497e2
fajduanihikam <fajduanihikam@gmail.com>
2024-05-21 03:10:25 +0700

    resolve development draft-4

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,2 @@
-#!/bin/bash
-
-# Update package list and install Python 3.9 and pip if they are not available
-apt-get update && apt-get install -y python3.9 python3.9-venv python3-pip
-
-apt-get install -y libpq-dev
-
-# Create and activate a virtual environment
-python3.9 -m venv venv
-source venv/bin/activate
-
-# Upgrade pip to the latest version
-pip install --upgrade pip
-
-# Install the dependencies from requirements.txt
-pip install -r requirements.txt
-
-# Collect static files
-python3.9 manage.py collectstatic --noinput
+pip install -r requirements.txt 
+python3.10.6 manage.py collectstatic
