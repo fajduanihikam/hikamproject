fda4c8f0d096ebe5551a931576b77e72c2e2c6af
fajduanihikam <fajduanihikam@gmail.com>
2024-05-21 03:56:02 +0700

    resolve development draft-10

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
-
-pip3 install psycopg2-binary
-
-# Install the necessary dependencies
-sudo apt-get update && sudo apt-get install -y libpq-dev
-
-# Install Python dependencies
+if [ -f /etc/alpine-release ]; then
+  apk add --no-cache libpq
+else
+  yum install -y libpq-dev
+fi
 pip3 install -r requirements.txt
-
-# Collect static files
 python3.9 manage.py collectstatic --noinput
-
-# Start the application
-gunicorn hikamproject.wsgi:application -b :8000
