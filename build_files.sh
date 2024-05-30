65a1895d567372c83459fca514587ca8c799ec99
fajduanihikam <fajduanihikam@gmail.com>
2024-05-30 13:57:31 +0700

    resolve development draft-11

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,3 @@
-if [ -f /etc/alpine-release ]; then
-  apk add --no-cache libpq
-else
-  yum install -y libpq-dev
-fi
-pip3 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+# build_files.sh
+pip install -r requirements.txt
+python3.10.6 manage.py collectstatic
